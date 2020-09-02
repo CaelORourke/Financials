fe6b9c84973107cac17e546aff78c46f25f938e9
Shane <shanegates@hotmail.com>
2020-09-02 14:39:54 -0700

    added Unsolved

---
+++ Unsolved/Financials.Data/FinancialsContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Financials.Data
+{
+    using Financials.Data.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class FinancialsContext : DbContext
+    {
+        public FinancialsContext(DbContextOptions<FinancialsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Transaction>().Property(e => e.TransactionId).HasColumnName("id");
+            modelBuilder.Entity<Transaction>().Property(e => e.TransactionValue).HasColumnName("transaction_value");
+            modelBuilder.Entity<Transaction>().Property(e => e.SoftDelete).HasColumnName("soft_delete");
+        }
+    }
+}
