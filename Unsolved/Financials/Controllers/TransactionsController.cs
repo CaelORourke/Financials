02d62c70fe088ace7dec03de498620c4d2c0204b
Shane <shanegates@hotmail.com>
2020-09-02 17:23:42 -0700

    add the starter TransactionsController.cs file

---
+++ Unsolved/Financials/Controllers/TransactionsController.cs
@@ -0,0 +1,34 @@
+﻿namespace Financials.Controllers
+{
+    using Financials.Data;
+    using Financials.Data.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly FinancialsContext _context;
+
+        public TransactionsController(FinancialsContext context)
+        {
+            _context = context;
+        }
+
+        #region Create
+        #endregion
+
+        #region Read
+        #endregion
+
+        #region Update
+        #endregion
+
+        #region Delete
+        #endregion
+    }
+}
